c97e0f939fd3b2e861ffdaadb2e3ee36f399bee8
İdil Gürsoy <64634611+idilsg@users.noreply.github.com>
2025-12-30 01:17:25 +0300

    Update DbConfig.java

--- src/data/DbConfig.java
+++ src/data/DbConfig.java
@@ -3,5 +3,5 @@ package data;
 public class DbConfig {
     public static final String URL = "jdbc:mysql://localhost:3306/mealdb?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     public static final String USER = "root";
-    public static final String PASS = "erid0704*";
+    public static final String PASS = "password";
 }
